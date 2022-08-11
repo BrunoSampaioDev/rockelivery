2f9fdf5541b4ae0008aa1549cc8631917f36de75
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-08-11 20:34:32 -0300

    feat(banking): add derive for view

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -6,6 +6,7 @@ defmodule Rockelivery.User do
   @primary_key {:id, :binary_id, autogenerate: true}
 
   @required_params [:address, :age, :cep, :cpf, :email, :password, :name]
+  @derive {Jason.Encoder, only: [:id, :age, :cpf, :address, :email]}
 
   schema "users" do
     field :address, :string
