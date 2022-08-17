df205fa2b0572468751a479c91345aa6ee65a34b
Bruno Sampaio <sampaionascimento100@gmail.com>
2022-08-16 21:21:56 -0300

    refactor(users): add update users module (#10)

--- lib/rockelivery_web/controllers/fallback_controller.ex
+++ lib/rockelivery_web/controllers/fallback_controller.ex
@@ -9,4 +9,11 @@ defmodule RockeliveryWeb.FallbackController do
     |> put_view(ErrorView)
     |> render("error.json", result: result)
   end
+
+  def call(conn, {:error, result}) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(ErrorView)
+    |> render("error.json", result: result)
+  end
 end
