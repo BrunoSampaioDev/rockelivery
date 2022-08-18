ac2771cdfd2e95d5a46117e75ecb7637e0b67388
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-08-18 18:53:04 -0300

    feat(users) normalize strings

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -3,9 +3,21 @@ defmodule Rockelivery.Users.Create do
 
   def call(params) do
     params
+    |> normalize_cep()
+    |> normalize_cpf()
     |> User.changeset()
     |> Repo.insert()
     |> handle_insert()
+  end
+
+  defp normalize_cpf(%{"cpf" => cpf} = params) do
+    normal = String.replace(cpf, ~r/[!#$%&()*+,.:;<=>?@\^_`{|}~-]/, "")
+    %{params | "cpf" => normal}
+  end
+
+  defp normalize_cep(%{"cep" => cep} = params) do
+    normal = String.replace(cep, ~r/[!#$%&()*+,.:;<=>?@\^_`{|}~-]/, "")
+    %{params | "cep" => normal}
   end
 
   defp handle_insert({:ok, %User{}} = result), do: result
