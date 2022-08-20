515dc26477f4fe0026ce612be439fea670ce4475
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-08-19 21:40:25 -0300

    refactor(banking): update user mocks

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,15 +1,29 @@
 defmodule Rockelivery.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Rockelivery.Repo
+  alias Rockelivery.User
 
-  def user_params_factory() do
+  def user_params_factory do
     %{
-      address: Faker.Address.PtBr.street_address(),
+      "address" => Faker.Address.PtBr.street_address(),
+      "age" => 31,
+      "cep" => Faker.Address.PtBr.zip_code(),
+      "cpf" => "13718203766",
+      "email" => "#{Faker.Person.PtBr.name()}@#{Faker.Internet.PtBr.free_email_service()}",
+      "password" => "123123",
+      "name" => Faker.Person.PtBr.name()
+    }
+  end
+
+  def user_factory do
+    %User{
+      address: "any_address",
       age: 31,
-      cep: Faker.Address.PtBr.zip_code(),
+      cep: "00000000",
       cpf: "13718203766",
-      email: "#{Faker.Person.PtBr.name()}@#{Faker.Internet.PtBr.free_email_service()}",
+      email: "any_mail@mail.com",
       password: "123123",
-      name: Faker.Person.PtBr.name()
+      name: "any name",
+      id: "84915c02-6537-4859-b609-87b2b9bf67a3"
     }
   end
 end
