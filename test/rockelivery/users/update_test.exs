dd0174f8d14667569eba078a8d7b2b6a76071986
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-08-22 20:19:01 -0300

    test(users): ensure update user

---
+++ test/rockelivery/users/update_test.exs
@@ -0,0 +1,41 @@
+defmodule Rockelivery.Users.UpdateTest do
+  use Rockelivery.DataCase, async: true
+
+  alias Rockelivery.{Error, User}
+  import Rockelivery.Factory
+  alias Rockelivery.Users.Update
+
+  describe "call/1" do
+    test "when are valid id, updated the user" do
+      insert(:user)
+
+      params =
+        build(:user_params, %{
+          "id" => "84915c02-6537-4859-b609-87b2b9bf67a3",
+          "email" => "new_mail@mail.com.br",
+          "address" => "new address 00",
+          "cpf" => "13718203761"
+        })
+
+      response = Update.call(params)
+
+      assert {:ok, %User{email: "new_mail@mail.com.br", address: "new address 00"}} = response
+    end
+
+    test "when are not valid id, returns the error" do
+      insert(:user)
+
+      params =
+        build(:user_params, %{
+          "id" => "84915c02-6537-4859-b609-87b2b9bf67a4",
+          "email" => "new_mail@mail.com.br",
+          "address" => "new address 00",
+          "cpf" => "13718203761"
+        })
+
+      response = Update.call(params)
+
+      assert {:error, %Error{result: "User not found", status: :not_found}} = response
+    end
+  end
+end
