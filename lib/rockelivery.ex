6289c4f37df83c083823a3c44ff58ce3ed588632
Bruno Sampaio <sampaionascimento100@gmail.com>
2022-08-25 20:35:44 -0300

    feat(items): add route and controller for create item (#20)

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,4 +1,6 @@
 defmodule Rockelivery do
+  alias Rockelivery.Items.Create, as: ItemCreate
+
   alias Rockelivery.Users.Create, as: UserCreate
   alias Rockelivery.Users.Delete, as: UserDelete
   alias Rockelivery.Users.Get, as: UserGet
@@ -11,4 +13,6 @@ defmodule Rockelivery do
   defdelegate delete_user(id), to: UserDelete, as: :call
 
   defdelegate update_user(params), to: UserUpdate, as: :call
+
+  defdelegate create_item(params), to: ItemCreate, as: :call
 end
