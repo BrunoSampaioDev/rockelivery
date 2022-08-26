f6dc1dff78b58e9c301434598094333d95642149
Bruno Sampaio <sampaionascimento100@gmail.com>
2022-08-25 21:01:26 -0300

    test(items): ensure render create item (#21)

---
+++ test/rockelivery_web/views/items_view_test.exs
@@ -0,0 +1,25 @@
+defmodule RockeliveryWeb.ItemsViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+  import Phoenix.View
+  import Rockelivery.Factory
+  alias Rockelivery.Item
+  alias RockeliveryWeb.ItemsView
+
+  test "renders create json" do
+    item = build(:item)
+
+    response = render(ItemsView, "create.json", item: item)
+
+    assert %{
+             message: "item created",
+             item: %Item{
+               category: :food,
+               description: "Pizza de banana",
+               id: "84915c02-6537-4859-b609-87b2b9bf67a3",
+               inserted_at: nil,
+               photo: "http://",
+               price: "12.50"
+             }
+           } == response
+  end
+end
