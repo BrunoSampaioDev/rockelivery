78d14a67fc61e04db14063b25e6b42e9ddf45931
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-08-29 19:37:23 -0300

    feat(items): validate items

--- lib/rockelivery/orders/create.ex
+++ lib/rockelivery/orders/create.ex
@@ -13,5 +13,14 @@ defmodule Rockelivery.Items.Order do
 
     query
     |> Repo.all()
+    |> validade_items(items_ids)
+  end
+
+  defp validade_items(items, items_ids) do
+    items_map = Map.new(items, fn item -> {item.id, item} end)
+
+    items_ids
+    |> Enum.map(fn id -> {id, Map.get(items_map, id)} end)
+    |> Enum.any?(fn {_id, value} -> is_nil(value) end)
   end
 end
