82604708c6d8ff4dd863c616a6cb667aaf746c22
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-09-03 20:31:15 -0300

    feat(users): validate cpf call

--- lib/via_cap/client.ex
+++ lib/via_cap/client.ex
@@ -11,8 +11,8 @@ defmodule Rockelivery.ViaCep.Client do
     |> handle_get()
   end
 
-  defp handle_get({:ok, %Env{status: 200, body: %{"erro" => true}}}) do
-    {:error, Error.build(:not_found, "CEP not found")}
+  defp handle_get({:ok, %Env{status: 200, body: %{"erro" => "true"}}}) do
+    {:error, Error.build(:not_found, "CEP not found!")}
   end
 
   defp handle_get({:ok, %Env{status: 400, body: _body}}) do
