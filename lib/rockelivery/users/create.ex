ff1bbb0e52030b1d2a96a37ae5d66fcc197799d2
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-09-04 16:45:26 -0300

    feat(users): validation

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,36 +1,22 @@
 defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.UsersHelpers
   alias Rockelivery.ViaCep.Client
 
   def call(%{"cep" => cep} = params) do
-    case Client.get_cep_info(cep) do
-      {:ok, _cep_info} -> create_user(params)
-      {:error, result} = error -> error
+    changeset =
+      params
+      |> UsersHelpers.normalize_cep()
+      |> UsersHelpers.normalize_cpf()
+      |> User.changeset()
+
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
-  end
-
-  defp create_user(params) do
-    params
-    |> normalize_cep()
-    |> normalize_cpf()
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
-
-  defp normalize_cpf(%{"cpf" => cpf} = params) do
-    normal = String.replace(cpf, ~r/[!#$%&()*+,.:;<=>?@\^_`{|}~-]/, "")
-    %{params | "cpf" => normal}
-  end
-
-  defp normalize_cep(%{"cep" => cep} = params) do
-    normal = String.replace(cep, ~r/[!#$%&()*+,.:;<=>?@\^_`{|}~-]/, "")
-    %{params | "cep" => normal}
-  end
-
-  defp handle_insert({:ok, %User{}} = result), do: result
-
-  defp handle_insert({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
   end
 end
